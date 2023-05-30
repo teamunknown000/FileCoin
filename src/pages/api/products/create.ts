604e1a6a90f63e1d51e12cd4becd45395e9059e7
notRyuk <amadeusrobot.notryuk@gmail.com>
2023-05-30 13:16:39 +0530

    Updated

---
+++ src/pages/api/products/create.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import Errors from "~/errors/main";
+import Product from "~/models/product";
+import { IApiResponse } from "~/types";
+import connect from "~/utils/db-connect";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<IApiResponse>
+) {
+    await connect()
+    const data = await Product.find()
+    res.status(data ? 200 : 400).send(Errors.generateResponse(data ? data : Errors.DB_ERROR, data !== undefined))
+}
